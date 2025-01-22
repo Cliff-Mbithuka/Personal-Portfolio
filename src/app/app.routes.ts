2f5817ce55d90c75d5a33a765915a8f1fbe522e9
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2025-01-22 22:39:16 +0300

    Add stack Module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,27 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'about',
-        loadChildren: () => import('./modules/navigation/navigation.module').then(m => m.NavigationModule)
-    },
-    {
-        path: 'project',
-        loadChildren: () => import('./modules/navigation/project/project.module').then(m => m.ProjectModule)
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./modules/navigation/navigation.module').then(
+        (m) => m.NavigationModule
+      ),
+  },
+  {
+    path: 'project',
+    loadChildren: () =>
+      import('./modules/navigation/project/project.module').then(
+        (m) => m.ProjectModule
+      ),
+  },
+  {
+    path: 'stack',
+    loadChildren: () =>
+      import('./modules/stack/stack.module').then((m) => m.StackModule),
+  },
 ];
